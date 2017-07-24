28655fdd1c5a3cdff3764d2fb587fe604a7d7995
James Patrick Kerrane <jamesjpkerrane@gmail.com>
2017-07-24 15:54:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * Theme - Stylesheet Generator for Reddit
+ * Authors: Sriram Krishnan, James Kerrane, Devonnae Williams
+ * Created: 7/20/2017
+ * Team: Owl 24
+ * Part of the Chingu Cohort
+*/
+
 function unhide(clickedButton, divID) {
 var item = document.getElementById(divID);
 if (item) {
