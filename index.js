61a5479013306286692c6bf56eab3af3498746fe
Sriram Krishnan <sriram.krishnan33@gmail.com>
2017-08-13 16:39:12 -0700

    reverted the dynamic loading changes

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ document.getElementById('Themes').addEventListener('change', function () {
 });
 
 
+	function unhide(clickedButton, divID) {
+	 var item = document.getElementById(divID);
+	 if (item) {
+	     if(item.className=='hidden'){
+	         item.className = 'unhidden' ;
+	        clickedButton.value = 'Back'
+	     }else{
+	         item.className = 'hidden';
+	         clickedButton.value = 'Legere'
+	     }
+	 }
+	 }
 
 
 $('#legere').click(function(){
