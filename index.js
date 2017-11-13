e080d277921b38c1c42dd753022de74278e8569b
jamesjpk123 <jamesjpkerrane@gmail.com>
2017-11-12 21:00:32 -0700

    Add pop-up support for 5 themes. Next 5 are coming soon.

--- index.js
+++ index.js
@@ -4,4 +4,48 @@
  * Created: 7/20/2017
  * Team: Owl 24
  * Part of the Chingu Cohort
-*/
+ */
+
+//Legere
+// When the user clicks the button, open the modal
+function legereOnClick() {
+  document.getElementById('legereModal').style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function legereClose() {
+  document.getElementById('legereModal').style.display = "none";
+}
+
+//Eddited
+// When the user clicks the button, open the modal
+function edditedOnClick() {
+  document.getElementById('edditedModal').style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function edditedClose() {
+  document.getElementById('edditedModal').style.display = "none";
+}
+
+//FBRT
+// When the user clicks the button, open the modal
+function fbrtOnClick() {
+  document.getElementById('fbrtModal').style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function fbrtClose() {
+  document.getElementById('fbrtModal').style.display = "none";
+}
+
+//Sequent
+// When the user clicks the button, open the modal
+function sequentOnClick() {
+  document.getElementById('sequentModal').style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function sequentClose() {
+  document.getElementById('sequentModal').style.display = "none";
+}
